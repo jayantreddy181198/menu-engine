e8f26d86aa8f597da5ff315d9022c776823a11b5
jayantreddy181198 <jayantreddy181198@gmail.com>
2024-05-02 11:16:03 +0530

    Adding .env support

--- menu-engine/settings.py
+++ menu-engine/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +82,11 @@ WSGI_APPLICATION = "menu-engine.wsgi.application"
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'boobootraunt',  # Name of your database
-        'USER': 'postgres',      # Your PostgreSQL user
-        'PASSWORD': 'postgres',  # Your PostgreSQL user's password
-        'HOST': '127.0.0.1',   # PostgreSQL server (use 'localhost' if running locally)
-        'PORT': '5433',        # PostgreSQL's default port
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
