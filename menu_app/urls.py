c6fa2cf6902dac26bd9e32911ea742e812f63a6a
jayantreddy181198 <jayantreddy181198@gmail.com>
2024-05-05 01:08:29 +0530

    Added Update api

--- menu_app/urls.py
+++ menu_app/urls.py
@@ -4,8 +4,10 @@ from .views import FoodItemCreateView, BarItemCreateView
 urlpatterns = [
     path('api/food/create', FoodItemCreateView.as_view(), name='food-item-create'),
     path('api/food/getall', FoodItemCreateView.as_view(), name='food-item-list'),
+    path('api/food/update', FoodItemCreateView.as_view(), name='food-item-update'),
     path('api/food/getall/<str:category>', FoodItemCreateView.as_view(), name='food-item-list'),
     path('api/bar/create', BarItemCreateView.as_view(), name='bar-item-create'),
     path('api/bar/getall', BarItemCreateView.as_view(), name='bar-item-list'),
-    path('api/bar/getall/<str:category>', BarItemCreateView.as_view(), name='bar-item-list')
+    path('api/bar/getall/<str:category>', BarItemCreateView.as_view(), name='bar-item-list'),
+    path('api/bar/update', BarItemCreateView.as_view(), name='bar-item-update')
 ]
--- menu_app/views.py
+++ menu_app/views.py
@@ -23,6 +23,34 @@ class FoodItemCreateView(APIView):
         serializer = FoodItemSerializer(food_items, many=True)
         return Response(serializer.data)
 
+    def put(self, request):
+        update_all = request.data.get('Update_All', False)
+        update_data = request.data.get('data', [])
+        updated_data = []
+        if update_all:
+            food_items = FoodItem.objects.all()
+            for food_item in food_items:
+                serializer = FoodItemSerializer(food_item, data=update_data, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    updated_data.append(serializer.data)
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(updated_data)
+        else:
+            ids_to_update = [item['id'] for item in update_data]
+            food_items = FoodItem.objects.filter(pk__in=ids_to_update)
+            for food_item in food_items:
+                for update in update_data:
+                    if update['id'] == food_item.id:
+                        serializer = FoodItemSerializer(food_item, data=update, partial=True)
+                        if serializer.is_valid():
+                            serializer.save()
+                            updated_data.append(serializer.data)
+                        else:
+                            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(updated_data)
+
 class BarItemCreateView(APIView):
     def post(self, request):
         serializer = BarItemSerializer(data=request.data, many=True)
@@ -38,3 +66,31 @@ class BarItemCreateView(APIView):
             bar_items = BarItem.objects.all()
         serializer = BarItemSerializer(bar_items, many=True)
         return Response(serializer.data)
+
+    def put(self, request):
+        update_all = request.data.get('Update_All', False)
+        update_data = request.data.get('data', [])
+        updated_data = []
+        if update_all:
+            bar_items = BarItem.objects.all()
+            for bar_item in bar_items:
+                serializer = BarItemSerializer(bar_item, data=update_data, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    updated_data.append(serializer.data)
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(updated_data)
+        else:
+            ids_to_update = [item['id'] for item in update_data]
+            bar_items = BarItem.objects.filter(pk__in=ids_to_update)
+            for bar_item in bar_items:
+                for update in update_data:
+                    if update['id'] == bar_item.id:
+                        serializer = BarItemSerializer(bar_item, data=update, partial=True)
+                        if serializer.is_valid():
+                            serializer.save()
+                            updated_data.append(serializer.data)
+                        else:
+                            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(updated_data)
